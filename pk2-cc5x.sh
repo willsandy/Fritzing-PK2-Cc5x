1642a2f5e26066136c180833783e0d81ab59f73f
Petter Arvidsson] <petter.arvidsson@gmail.com>
2015-12-30 19:52:13 +0100

    Use new arguments and create readme
    - Use new arguments from Fritzing
    - Create instructions for linux usage

--- pk2-cc5x.sh
+++ pk2-cc5x.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wine $1cc5x/CC5X.EXE -I$1/cc5x $3 -o$1hex.out && $1pk2cmdv1-20Linux2-6/pk2cmd -B$1pk2cmdv1-20Linux2-6 -P$2 -M -F$1out.hex
+wine $1Cc5x/CC5X.EXE -I$1/Cc5x $3$4 -o$3$5.hex && $1PK2Cmd/pk2cmdv1-20Linux2-6/pk2cmd -B$1PK2Cmd/pk2cmdv1-20Linux2-6 -P$2 -M -F$3$5.hex
