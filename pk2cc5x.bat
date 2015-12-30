ef2d3a61be27a178d2c5dd9c1ba0825e124b2ce1
William Sandqvist <william@kth.se>
2015-12-30 18:27:51 +0100

    Use new arguments passed by fritzing

--- pk2cc5x.bat
+++ pk2cc5x.bat
@@ -1,22 +1,16 @@
 : REM BAT-file name pk2cc5x.bat
 : REM %1=C:\PK2proj\                     path to the tool-chain
 : REM %2=PIC16f690                       pic device name
-: REM %3=C:\PK2proj\Work\hello_blink.c   path to c-sourcefile
+: REM %3=C:\PK2proj\Work\                path to work directory
+: REM %4=hello_blink.c                   File name
+: REM %5=hello_blink                     File name without extension
 
 @ECHO.
 @ECHO -NOW COMPILING WITH CC5X
 
-
-%1Cc5x\Cc5x.exe %3 -O%1Work\ -a
+"%~1Cc5x\Cc5x.exe" "%~3%~4" -I"%~1Cc5x" -O%3 -a
 
 @ECHO.
 @ECHO -NOW DOWNLOADING CODE WITH PK2CMD
 
-: REM derive name of hex-file from path to c-file
-@echo off
-Set filename=%3
-For %%A in ("%filename%") do (Set Name=%%~nxA)
-set hexfile=%Name:.c=.hex%
-echo on
-
-%1PK2Cmd\pk2cmd -b%1PK2Cmd\ -p%2 -f%1Work\%hexfile% -a4.5 -m -r -t -jn
+"%~1PK2Cmd\pk2cmd" -b"%~1PK2Cmd" -p%2 -f"%~3%~5.hex" -a4.5 -m -r -t -jn
